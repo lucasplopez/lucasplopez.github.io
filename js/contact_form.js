14b48ac3aba534ef0e3a978a0f7a069036e4a71f
MaxiJGomez <86429830+MaxiJGomez@users.noreply.github.com>
2021-12-27 09:24:09 -0300

    Add files via upload

---
+++ js/contact_form.js
@@ -0,0 +1,74 @@
+export function ContactForm() {
+  const d = document;
+
+  function validationsForm() {
+    const $form = d.querySelector(".contact-form"),
+      $inputs = d.querySelectorAll(".contact-form [required]");
+
+    $inputs.forEach((input) => {
+      const $span = d.createElement("span");
+      $span.id = input.name;
+      $span.textContent = input.title;
+      $span.classList.add("contact-form-error", "none");
+      input.insertAdjacentElement("afterend", $span);
+    });
+
+    d.addEventListener("keyup", (e) => {
+      if (e.target.matches(".contact-form [required]")) {
+        let $input = e.target,
+          pattern = $input.pattern || $input.dataset.pattern;
+        if (pattern && $input.value !== "") {
+          let regex = new RegExp(pattern);
+          return !regex.exec($input.value)
+            ? d.getElementById($input.name).classList.add("is-active")
+            : d.getElementById($input.name).classList.remove("is-active");
+        }
+
+        if (!pattern) {
+          return $input.value === ""
+            ? d.getElementById($input.name).classList.add("is-active")
+            : d.getElementById($input.name).classList.remove("is-active");
+        }
+      }
+    });
+
+    d.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const $loader = d.querySelector(".contact-form-loader"),
+        $response = d.querySelector(".contact-form-response");
+
+      $loader.classList.remove("none");
+
+      fetch("https://formsubmit.co/ajax/mjg_04_10@hotmail.com", {
+        method: "POST",
+        body: new FormData(e.target),
+      })
+        .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+        .then((json) => {
+          // console.log(json);
+          $loader.classList.add("none");
+          $response.classList.remove("none");
+          $response.innerHTML = `<p>El mensaje ha sido enviado correctamente</p>`;
+          // $response.innerHTML = `<p>${json.message}</p>`;
+          $form.reset();
+        })
+        .catch((err) => {
+          console.errorlog(err);
+          let message =
+            err.statusText || "Ocurrio un error al enviar, intenta nuevamente";
+          $response.innerHTML = `<p>Error ${error.status}: ${message}</p>`;
+        })
+        .finally(() =>
+          setTimeout(() => {
+            $response.classList.add("none");
+            $response.innerHTML = "";
+          }, 3000)
+        );
+    });
+  }
+
+  setTimeout(() => validationsForm(), 100);
+
+  return $form;
+}
