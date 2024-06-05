5e6469ab6a36c6f12a374e5347b31abe76012743
Lucas Lopez <lucas.p.lopez.pj@gmail.com>
2024-06-05 12:07:05 -0300

    actualizo

--- js/funciones.js
+++ js/funciones.js
@@ -40,3 +40,85 @@ function updateClock() {
       dateElement.textContent = formattedDate;
     });
   }
+
+
+  class GalleryComponent extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: 'open' });
+      this.shadowRoot.innerHTML = `
+        <style>
+          .gallery {
+            display: none;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: rgba(0, 0, 0, 0.9);
+            color: white;
+            justify-content: center;
+            align-items: center;
+            z-index: 1000;
+          }
+          .gallery-content {
+            max-width: 90%;
+            max-height: 90%;
+            overflow: auto;
+          }
+          .gallery img, .gallery html {
+            max-width: 100%;
+            max-height: 100%;
+          }
+          .gallery.active {
+            display: flex;
+          }
+        </style>
+        <div class="gallery">
+          <div class="gallery-content"></div>
+        </div>
+      `;
+      this.gallery = this.shadowRoot.querySelector('.gallery');
+      this.galleryContent = this.shadowRoot.querySelector('.gallery-content');
+    }
+  
+    connectedCallback() {
+      document.querySelectorAll('[data-gallery]').forEach(item => {
+        item.addEventListener('click', (e) => this.openGallery(e));
+      });
+      this.gallery.addEventListener('click', () => this.closeGallery());
+    }
+  
+    openGallery(event) {
+      const galleryId = event.target.dataset.gallery;
+      this.renderGalleryContent(galleryId);
+      this.gallery.classList.add('active');
+    }
+  
+    closeGallery() {
+      this.gallery.classList.remove('active');
+    }
+  
+    renderGalleryContent(galleryId) {
+      let content = '';
+      switch(galleryId) {
+        case 'gallery1':
+          content = '<img src="img/gallery1.png" alt="Gallery 1">';
+          break;
+        case 'gallery2':
+          content = '<img src="img/gallery2.jpg" alt="Gallery 2">';
+          break;
+        case 'gallery3':
+          content = '<img src="img/gallery3.jpg" alt="Gallery 3">';
+          break;
+        case 'gallery4':
+          content = '<img src="img/gallery4.jpg" alt="Gallery 4">';
+          break;
+        default:
+          content = '<p>Galeria</p>';
+      }
+      this.galleryContent.innerHTML = content;
+    }
+  }
+  
+  customElements.define('gallery-component', GalleryComponent);
